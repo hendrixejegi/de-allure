51b64e8491b013fbbbafa33efb0c0622d4da5591
Hendrix Ejegi <tosanwumiae@gmail.com>
2025-12-25 02:59:28 +0100

    add custom api-types package

---
+++ packages/api-types/index.d.ts
@@ -0,0 +1,11 @@
+declare module '@de-allure/api-types' {
+  // Type definitions for [~THE LIBRARY NAME~] [~OPTIONAL VERSION NUMBER~]
+  // Project: [~THE PROJECT NAME~]
+  // Definitions by: [~YOUR NAME~] <[~A URL FOR YOU~]>
+
+  export interface ApiResponse<T> {
+    success: boolean;
+    message: string;
+    data?: T;
+  }
+}
